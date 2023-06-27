d7c50effeba7b758e00e905312c479c3d811da0f
Tom de Geus <tom@geus.me>
2023-06-27 11:46:54 +0200

    Thermal avalanches: measuring "x"

--- mycode_thermal_epm/Thermal.py
+++ mycode_thermal_epm/Thermal.py
@@ -11,6 +11,7 @@ import numpy as np
 import tqdm
 
 from . import Preparation
+from . import storage
 from . import tag
 from . import tools
 from ._version import version
@@ -18,7 +19,9 @@ from .Preparation import data_version
 
 f_info = "EnsembleInfo.h5"
 m_name = "Thermal"
+m_avalanche = "Avalanche"
 m_exclude = ["AQS", "Extremal", "ExtremalAvalanche"]
+m_dummy_int = int(12e12)
 
 
 class SystemThermalStressControl(epm.SystemThermalStressControl):
@@ -45,37 +48,109 @@ class SystemThermalStressControl(epm.SystemThermalStressControl):
         self.temperature = param["temperature"][...]
 
 
+def _clean_data_v3(src: h5py.File, dst: h5py.File) -> bool:
+    """
+    Remove ``idx_ignore`` and ``x_ignore`` if they contain only dummy values.
+    :return: True if any data was removed, False otherwise.
+    """
+    root = src[m_name][m_avalanche]
+    i = False
+    x = False
+    if "idx_ignore" in root:
+        i = np.all(root["idx_ignore"][...] == m_dummy_int)
+    if "x_ignore" in root:
+        x = np.all(root["x_ignore"][...] == m_dummy_int)
+    if not (i or x):
+        return False
+
+    g5.copy(src, dst, ["/param", "/restart", "/meta"])
+    paths = g5.getdatapaths(src, root=f"/{m_name}")
+    if i:
+        paths.remove(f"/{m_name}/{m_avalanche}/idx_ignore")
+    if x:
+        paths.remove(f"/{m_name}/{m_avalanche}/x_ignore")
+    g5.copy(src, dst, paths)
+    return True
+
+
+def _upgrade_data_v2_to_v3(src: h5py.File, dst: h5py.File):
+    """
+    -   'Avalanche' data: rename ``.../...`` to ``.../Avalanche/...``.
+    -   Convert ``.../T`` to ``.../Avalanche/t`` as real time.
+    -   Deprecated ``.../mean_epsp``.
+    -   Add ``.../Avalanche/x`` and ``.../Avalanche/x_ignore``.
+    -   Add restart option after avalanche, for possible future use.
+    """
+    g5.copy(src, dst, ["/param", "/restart", "/meta"])
+
+    rename = {key: key for key in g5.getdatapaths(src, root=f"/{m_name}")}
+    rename.pop(f"/{m_name}/mean_epsp")
+    rename[f"/{m_name}/T"] = f"/{m_name}/{m_avalanche}/t"
+    rename[f"/{m_name}/idx"] = f"/{m_name}/{m_avalanche}/idx"
+    if f"/{m_name}/idx_ignore" in src:
+        rename[f"/{m_name}/idx_ignore"] = f"/{m_name}/{m_avalanche}/idx_ignore"
+    g5.copy(src, dst, [i for i in rename.keys()], [i for i in rename.values()])
+
+    basic = dst[m_name]
+    root = dst[f"/{m_name}/{m_avalanche}"]
+    shape = root["idx"].shape
+    n = shape[0]
+
+    dset = root["t"]
+    for i in range(n):
+        dset[i, :] += src[f"/{m_name}/t"][i]
+
+    dset = root.create_dataset("x", shape, maxshape=(None, shape[1]), dtype=np.float64)
+    dset[:] = np.nan
+
+    dset = root.create_dataset("x_ignore", (n,), maxshape=(None,), dtype=np.uint64)
+    dset[:] = np.arange(n, dtype=np.uint64)
+
+    restart = root.create_group("restart")
+    for key in ["t", "epsp", "sigma", "sigmay", "state"]:
+        restart.create_dataset(
+            key,
+            basic[key].shape,
+            maxshape=[None for _ in range(basic[key].ndim)],
+            dtype=basic[key].dtype,
+        )
+    restart["t"][:] = -1
+
+
 def _upgrade_data_v1_to_v2(src: h5py.File, dst: h5py.File):
+    """
+    -   Convert ``A`` to ``idx``, add ``idx_ignore`` for those data.
+    """
     g5.copy(src, dst, ["/param", "/restart"])
     Preparation._copy_metadata_pre_v2(src, dst)
 
-    if "Thermal" not in src:
+    if m_name not in src:
         return
 
-    A = src["/Thermal/A"][...]
-    if "idx" in src["Thermal"]:
-        idx = src["/Thermal/idx"][...]
+    A = src[f"/{m_name}/A"][...]
+    if "idx" in src[m_name]:
+        idx = src[f"/{m_name}/idx"][...]
         n = A.shape[0] - idx.shape[0]
     else:
         idx = None
         n = 0
-    cp = g5.getdatapaths(src, root="/Thermal")
-    cp.remove("/Thermal/A")
+    cp = g5.getdatapaths(src, root=f"/{m_name}")
+    cp.remove(f"/{m_name}/A")
 
     if n == 0:
         g5.copy(src, dst, cp)
-        root = dst["/Thermal"]
+        root = dst[f"/{m_name}"]
         assert root["t"].maxshape[0] is None
         if idx is None:
-            g5.copy(src, dst, "/Thermal/A", "/Thermal/idx")
+            g5.copy(src, dst, f"/{m_name}/A", f"/{m_name}/idx")
             n = A.shape[0]
             dset = root.create_dataset("idx_ignore", (n,), maxshape=(None,), dtype=np.uint64)
             dset[:] = np.arange(n, dtype=np.uint64)
         return
 
-    cp.remove("/Thermal/idx")
+    cp.remove(f"/{m_name}/idx")
     g5.copy(src, dst, cp)
-    root = dst["/Thermal"]
+    root = dst[f"/{m_name}"]
     assert root["t"].maxshape[0] is None
 
     dset = root.create_dataset("idx", A.shape, maxshape=(None, A.shape[1]), dtype=idx.dtype)
@@ -90,6 +165,16 @@ def _upgrade_data_v1_to_v2(src: h5py.File, dst: h5py.File):
     dset[:] = np.arange(n, dtype=np.uint64)
 
 
+def _upgrade_data_to_v1(src: h5py.File, dst: h5py.File):
+    paths = g5.getdatapaths(src)
+    paths.remove(f"/{m_name}/S")
+    g5.copy(src, dst, paths)
+
+    S = src[f"/{m_name}/S"][...]
+    expect = np.arange(S.shape[1]).reshape((1, -1))
+    assert np.all(np.equal(S, expect)), "S must arange"
+
+
 def _upgrade_data(filename: pathlib.Path, temp_dir: pathlib.Path) -> bool:
     """
     Upgrade data to the current version.
@@ -102,21 +187,17 @@ def _upgrade_data(filename: pathlib.Path, temp_dir: pathlib.Path) -> bool:
         assert not any(x in src for x in m_exclude + Preparation._libname_pre_v2(m_exclude))
         ver = Preparation.get_data_version(src)
 
-    if tag.greater_equal(ver, "2.0"):
-        return None
+    if tag.greater_equal(ver, "3.0"):
+        temp_file = temp_dir / "clean.h5"
+        with h5py.File(filename) as src, h5py.File(temp_file, "w") as dst:
+            if _clean_data_v3(src, dst):
+                return temp_file
+            return None
 
     if tag.less(ver, "1.0"):
         temp_file = temp_dir / "from_1_0.h5"
         with h5py.File(filename) as src, h5py.File(temp_file, "w") as dst:
-            paths = g5.getdatapaths(src)
-            paths.remove("/Thermal/S")
-
-            S = src["Thermal/S"][...]
-            expect = np.arange(S.shape[1]).reshape((1, -1))
-            assert np.all(np.equal(S, expect)), "S must arange"
-
-            g5.copy(src, dst, paths)
-            dst["/param/data_version"] = data_version
+            _upgrade_data_to_v1(src, dst)
         filename = temp_file
         ver = "1.0"
 
@@ -124,6 +205,18 @@ def _upgrade_data(filename: pathlib.Path, temp_dir: pathlib.Path) -> bool:
         temp_file = temp_dir / "from_2_0.h5"
         with h5py.File(filename) as src, h5py.File(temp_file, "w") as dst:
             _upgrade_data_v1_to_v2(src, dst)
+        filename = temp_file
+        ver = "2.0"
+
+    if tag.less(ver, "3.0"):
+        temp_file = temp_dir / "from_3_0.h5"
+        with h5py.File(filename) as src, h5py.File(temp_file, "w") as dst:
+            _upgrade_data_v2_to_v3(src, dst)
+        filename = temp_file
+        ver = "3.0"
+
+    with h5py.File(temp_file, "a") as dst:
+        storage.dump_overwrite(dst, "/param/data_version", data_version)
 
     return temp_file
 
@@ -182,6 +275,71 @@ def BranchPreparation(cli_args=None):
         tools.create_check_meta(dest, f"/meta/{m_name}/{funcname}", dev=args.develop)
 
 
+def ComputeMissing(cli_args=None):
+    """
+    Rerun to compute missing data.
+    """
+
+    class MyFmt(
+        argparse.RawDescriptionHelpFormatter,
+        argparse.ArgumentDefaultsHelpFormatter,
+        argparse.MetavarTypeHelpFormatter,
+    ):
+        pass
+
+    funcname = inspect.getframeinfo(inspect.currentframe()).function
+    doc = textwrap.dedent(inspect.getdoc(globals()[funcname]))
+    parser = argparse.ArgumentParser(formatter_class=MyFmt, description=doc)
+
+    parser.add_argument("--develop", action="store_true", help="Allow uncommitted")
+    parser.add_argument("-v", "--version", action="version", version=version)
+    parser.add_argument("file", type=pathlib.Path, help="Input/output file")
+
+    args = tools._parse(parser, cli_args)
+    assert args.file.exists()
+
+    with h5py.File(args.file, "a") as file:
+        assert Preparation.get_data_version(file) == data_version
+        tools.create_check_meta(file, f"/meta/{m_name}/{funcname}", dev=args.develop)
+        system = SystemThermalStressControl(file)
+        snapshot = file[m_name]
+        avalanche = snapshot[m_avalanche]
+        restart = avalanche["restart"]
+        ninc = avalanche["idx"].shape[1]
+        ignore_x = avalanche["x_ignore"][...]
+        ignore_idx = avalanche["idx_ignore"][...]
+        for j, i in enumerate(tqdm.tqdm(ignore_x)):
+            if i == m_dummy_int:
+                continue
+            system.epsp = snapshot["epsp"][i, ...]
+            system.sigma = snapshot["sigma"][i, ...]
+            system.sigmay = snapshot["sigmay"][i, ...]
+            system.t = snapshot["t"][i]
+            system.state = snapshot["state"][i]
+
+            avalanche = epm.Avalanche()
+            avalanche.makeThermalFailureSteps(system, ninc)
+            assert np.allclose(avalanche.t, avalanche["t"][i, :])
+            if i not in ignore_idx:
+                assert np.all(np.equal(avalanche.idx, avalanche["idx"][i, :]))
+            else:
+                avalanche["idx"][i, :] = avalanche.idx
+                avalanche["idx_ignore"][np.argmax(ignore_idx == i)] = m_dummy_int
+            avalanche["x"][i, :] = avalanche.x
+            avalanche["x_ignore"][j] = m_dummy_int
+
+            restart["epsp"][i, ...] = system.epsp
+            restart["sigma"][i, ...] = system.sigma
+            restart["sigmay"][i, ...] = system.sigmay
+            restart["t"][i] = system.t
+            restart["state"][i] = system.state
+            file.flush()
+
+        assert np.all(restart["t"][...] > 0)
+        assert np.all(avalanche["x_ignore"][...] == m_dummy_int)
+        assert np.all(avalanche["idx_ignore"][...] == m_dummy_int)
+
+
 def Run(cli_args=None):
     """
     Run simulation at fixed stress.
@@ -218,9 +376,10 @@ def Run(cli_args=None):
     assert args.file.exists()
 
     with h5py.File(args.file, "a") as file:
+        assert Preparation.get_data_version(file) == data_version
         tools.create_check_meta(file, f"/meta/{m_name}/{funcname}", dev=args.develop)
         system = SystemThermalStressControl(file)
-        restart = file["restart"]
+        global_restart = file["restart"]
         if args.ninc is None:
             args.ninc = 20 * system.size
         else:
@@ -228,11 +387,13 @@ def Run(cli_args=None):
 
         if m_name not in file:
             assert not any(m in file for m in m_exclude), "Wrong file type"
-            res = file.create_group(m_name)
-            ava = res.create_group("avalanches")
+            snapshot = file.create_group(m_name)
+            avalanche = snapshot.create_group(m_avalanche)
+            restart = avalanche.create_group("restart")
         else:
-            res = file[m_name]
-            ava = res["avalanches"]
+            snapshot = file[m_name]
+            avalanche = snapshot[m_avalanche]
+            restart = avalanche["restart"]
 
         for _ in tqdm.tqdm(range(args.measurements), desc=str(args.file)):
             nfails = system.nfails.copy()
@@ -242,32 +403,47 @@ def Run(cli_args=None):
                     break
                 system.makeThermalFailureSteps(system.size)
 
-            with g5.ExtendableSlice(res, "epsp", system.shape, np.float64) as dset:
+            with g5.ExtendableSlice(snapshot, "epsp", system.shape, np.float64) as dset:
                 dset += system.epsp
-            with g5.ExtendableSlice(res, "sigma", system.shape, np.float64) as dset:
+            with g5.ExtendableSlice(snapshot, "sigma", system.shape, np.float64) as dset:
                 dset += system.sigma
-            with g5.ExtendableSlice(res, "sigmay", system.shape, np.float64) as dset:
+            with g5.ExtendableSlice(snapshot, "sigmay", system.shape, np.float64) as dset:
                 dset += system.sigmay
-            with g5.ExtendableList(res, "state", np.uint64) as dset:
+            with g5.ExtendableList(snapshot, "state", np.uint64) as dset:
                 dset.append(system.state)
-            with g5.ExtendableList(res, "t", np.float64) as dset:
+            with g5.ExtendableList(snapshot, "t", np.float64) as dset:
                 dset.append(system.t)
 
             if args.ninc > 0:
-                avalanche = epm.Avalanche()
-                avalanche.makeThermalFailureSteps(system, args.ninc)
-                with g5.ExtendableSlice(ava, "idx", [args.ninc], np.uint64) as dset:
-                    dset += avalanche.idx
-                with g5.ExtendableSlice(ava, "x", [args.ninc], np.float64) as dset:
-                    dset += avalanche.x
-                with g5.ExtendableSlice(ava, "t", [args.ninc], np.float64) as dset:
-                    dset += avalanche.t
+                # Measure avalanche
+                measurement = epm.Avalanche()
+                measurement.makeThermalFailureSteps(system, args.ninc)
 
-            restart["epsp"][...] = system.epsp
-            restart["sigma"][...] = system.sigma
-            restart["sigmay"][...] = system.sigmay
-            restart["t"][...] = system.t
-            restart["state"][...] = system.state
+                # Store basic quantities
+                with g5.ExtendableSlice(avalanche, "idx", [args.ninc], np.uint64) as dset:
+                    dset += measurement.idx
+                with g5.ExtendableSlice(avalanche, "x", [args.ninc], np.float64) as dset:
+                    dset += measurement.x
+                with g5.ExtendableSlice(avalanche, "t", [args.ninc], np.float64) as dset:
+                    dset += measurement.t
+
+                # Store restart info if the avalanche should be extended in the future
+                with g5.ExtendableSlice(restart, "epsp", system.shape, np.float64) as dset:
+                    dset += system.epsp
+                with g5.ExtendableSlice(restart, "sigma", system.shape, np.float64) as dset:
+                    dset += system.sigma
+                with g5.ExtendableSlice(restart, "sigmay", system.shape, np.float64) as dset:
+                    dset += system.sigmay
+                with g5.ExtendableList(restart, "state", np.uint64) as dset:
+                    dset.append(system.state)
+                with g5.ExtendableList(restart, "t", np.float64) as dset:
+                    dset.append(system.t)
+
+            global_restart["epsp"][...] = system.epsp
+            global_restart["sigma"][...] = system.sigma
+            global_restart["sigmay"][...] = system.sigmay
+            global_restart["t"][...] = system.t
+            global_restart["state"][...] = system.state
             file.flush()
 
 
@@ -306,18 +482,19 @@ def EnsembleInfo(cli_args=None):
         tmax = 0
         for f in tqdm.tqdm(args.files):
             with h5py.File(f) as file:
+                assert Preparation.get_data_version(file) == data_version
                 assert m_name in file, "Wrong file type"
                 assert not any(m in file for m in m_exclude), "Wrong file type"
-                res = file[m_name]
-                ava = res["avalanches"]
-                for i in range(ava["t"].shape[0]):
-                    tmin = min(tmin, ava["t"][i, 0] - res["t"][i])
-                    tmax = max(tmax, ava["t"][i, -1] - res["t"][i])
+                snapshot = file[m_name]
+                avalanche = snapshot[m_avalanche]
+                for i in range(avalanche["t"].shape[0]):
+                    tmin = min(tmin, avalanche["t"][i, 0] - snapshot["t"][i])
+                    tmax = max(tmax, avalanche["t"][i, -1] - snapshot["t"][i])
 
         for ifile, f in enumerate(tqdm.tqdm(args.files)):
             with h5py.File(f) as file:
-                res = file[m_name]
-                ava = res["avalanches"]
+                snapshot = file[m_name]
+                avalanche = snapshot[m_avalanche]
 
                 if ifile == 0:
                     g5.copy(file, output, ["/param"])
@@ -333,10 +510,10 @@ def EnsembleInfo(cli_args=None):
                     Asq = enstat.binned(bin_edges, names=["x", "y"], bound_error="ignore")
                     N = np.prod(file["param"]["shape"][...])
 
-                pdfx += (res["sigmay"][...] - np.abs(res["sigma"][...])).ravel()
-                for i in range(res["t"].shape[0]):
-                    ti = ava["t"][i, ...] - res["t"][i]
-                    ai = epm.cumsum_n_unique(ava["idx"][i, ...]) / N
+                pdfx += (snapshot["sigmay"][...] - np.abs(snapshot["sigma"][...])).ravel()
+                for i in range(snapshot["t"].shape[0]):
+                    ti = avalanche["t"][i, ...] - snapshot["t"][i]
+                    ai = epm.cumsum_n_unique(avalanche["idx"][i, ...]) / N
                     si = np.arange(1, ti.size + 1) / N
                     S.add_sample(ti, si)
                     Ssq.add_sample(ti, si**2)
